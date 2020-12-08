ff1ab29080b920220d492bdda064fe337683a218
Jefferson Souza <hsinfo@gmail.com>
2020-12-08 12:59:30 +0100

    Resolvida questão 10-a do AT de Python

--- fundamentos-python/AT/10-a.py
+++ fundamentos-python/AT/10-a.py
@@ -10,24 +10,15 @@
 
 import pandas as pd
 
-medalhas_olimpicas = pd.read_csv('https://sites.google.com/site/dr2fundamentospython/arquivos/Winter_Olympics_Medals.csv')
-
 paises = ['NOR', 'SWE', 'FIN']
+modalidades = ['Skiing', 'Curling', 'Skating', 'Ice Hockey']
 
-medalhas_olimpicas = medalhas_olimpicas.loc[
-    (medalhas_olimpicas['NOC'] == 'NOR') |
-    (medalhas_olimpicas['NOC'] == 'SWE') |
-    (medalhas_olimpicas['NOC'] == 'FIN')
-]
+medalhas_olimpicas = pd.read_csv('https://sites.google.com/site/dr2fundamentospython/arquivos/Winter_Olympics_Medals.csv')
+medalhas_olimpicas = medalhas_olimpicas\
+    .query("NOC in @paises and Sport in @modalidades and Year > 2000 and Medal == 'Gold'")\
+    .groupby(['NOC'], as_index=False)\
+    .count()
 
-medalhas_olimpicas = medalhas_olimpicas.loc[(medalhas_olimpicas['Year'] > 2000) & (medalhas_olimpicas['Medal'] == 'Gold')]
-medalhas_olimpicas = medalhas_olimpicas.loc[
-    (medalhas_olimpicas['Sport'] == 'Skiing') |
-    (medalhas_olimpicas['Sport'] == 'Curling') |
-    (medalhas_olimpicas['Sport'] == 'Skating') |
-    (medalhas_olimpicas['Sport'] == 'Ice Hockey')
-]
+medalhista = medalhas_olimpicas.sort_values(by=['Medal'], ascending=False).iloc[0]
 
-medalhas_olimpicas = medalhas_olimpicas.groupby(['Medal']).count()
-
-print(medalhas_olimpicas)
+print(f"O maior medalhista de ouro do século 21 nas modalidades Skiing, Curling, Skating, Ice Hockey foi: {medalhista['NOC']} com {medalhista['Medal']} medalhas")
